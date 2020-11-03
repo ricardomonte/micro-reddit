91cee0ddcc3ad2827b54f950fdf9fe0f3827a013
Kingsley <kingobaino@gmail.com>
2020-11-03 21:45:04 +0100

    Add the comment model and validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 5, maximum: 10}
-  validates :body, presence: true, length: {minimum: 10, maximum: 20}
+  has_many :comments
+  validates :title, presence: true, length: { minimum: 5, maximum: 10 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_200712) do
+ActiveRecord::Schema.define(version: 2020_11_03_203112) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -27,5 +35,6 @@ ActiveRecord::Schema.define(version: 2020_11_03_200712) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
